9fead3bc17a4d13b30af34de99cfb2de3e5b1156
hushbrush <harshitachakravadhanula@gmail.com>
2024-09-16 01:05:00 -0400

    trying to close paths

--- app.js
+++ app.js
@@ -62,6 +62,14 @@ function zones(hours)
     }
 }
 
+function drawOcts(octpoints, oct, colour)
+{
+    oct= svg.append('path');
+    oct
+    .attr('d', `M${octpoints.map(p => p.join(',')).join('L')}Z`)
+    .attr('fill', colour)
+    .attr('opacity', 0.5);
+}
 
 function loop()
  {
@@ -75,36 +83,21 @@ function loop()
     const tri =svg.append("path");
 
     zones(5);
-    
+    const oct1 = null, oct2 = null, oct3 = null;
 
-    
-    const oct3= svg.append('path');
-    oct3
-    .attr('d', `M${octpoints3.map(p => p.join(',')).join('L')}Z`)
-    .attr('fill', `purple`)
-    .attr('opacity', 0.5);
-     
-    drawHours(zone3hour, (radius*3));
+    drawOcts(octpoints3, oct3, 'purple');
+     drawHours(zone3hour, (radius*3));
 
 
-    const oct2= svg.append('path');
-    oct2
-    .attr('d', `M${octpoints2.map(p => p.join(',')).join('L')}Z`)
-    .attr('fill', `teal`)
-    .attr('opacity', 0.5);
-        
+  
+    drawOcts(octpoints2, oct2, 'teal');
     drawHours(zone2hour, (radius*2));
 
 
-    const oct1= svg.append('path');
-    oct1
-    .attr('d', `M${octpoints1.map(p => p.join(',')).join('L')}Z`)
-    .attr('fill', 'black')
-    .attr('opacity', 0.5);
 
+    drawOcts(octpoints1, oct1, 'black');
+    drawHours(zone1hour, radius );
 
-   drawHours(zone1hour, radius );
-console.log(zone1hour, zone2hour, zone3hour)
 
 
     window.requestAnimationFrame(loop);
@@ -143,93 +136,3 @@ function drawHours(hour_normalised, radius)
 }
 
 
-
-// function drawOcts(radius, color)
-// {
-//     for (let i = 0; i < 8; i++)
-//         {
-//             const angle = (Math.PI / 4) * i; // Angle for each vertex
-//             const x = cx + radius * Math.cos(angle);
-//             const y = cy + radius * Math.sin(angle);
-//             octpoints1.push([x, y]);
-//             // const x2 = cx + (radius*2) * Math.cos(angle);
-//             // const y2 = cy + (radius*2) * Math.sin(angle);
-//             // octpoints2.push([x2, y2]);
-//             // const x3 = cx + (radius*3) * Math.cos(angle);
-//             // const y3 = cy + (radius*3) * Math.sin(angle);
-//             // octpoints3.push([x3, y3]);
-//         }
-//    // const oct1= svg.append('path');
-//     svg.append
-//     .attr('d', `M${octpoints1.map(p => p.join(',')).join('L')}Z`)
-//     .attr('fill', color)
-//     .attr(opacity, '0.5');
-//     // const oct2= svg.append('path');
-//     // const oct3= svg.append('path');
-
-// }
-
-
-  // // draw unfinished hour: 
-    // // zone 1 minutes
-    // //figure out how to make this the hour angle later
-    
-   
-    // for(var i=0; i<=min_normalised; i++)
-    // {
-        
-    //     const x = cx + radius * Math.cos(angle);
-    //     const y = cy + radius * Math.sin(angle);
-    //     console.log("angle: "+angle);
-    //     angle = angle+(Math.PI/(60*4)); // Angle for each vertex: 60 parts of eact oct
-    //     minpoints.push([x, y]);
-
-    //     //draw a circle at minpoints
-        
-    //     // svg.append('circle')
-    //     //     .attr('x', minpoint[key][0])
-    //     //     .attr('y', minpoint[key][1])
-    //     //     .attr('r', 100)
-    //     //     .attr('fill', 'red')
-    //     //     .attr('opacity', '0.5'); 
-    // }
-
-
-    // //finding the last point of the hour to start drawing the minutes on
-    // point1=hourpoints[hourpoints.length-1];
-    // point2=minpoints[0];
-   
-
-
-
-    // // for( var j=0; j<min_normalised+2; j++)
-    // //     {
-    // //         //draw the triangle that divides the oct into 60 parts
-    // //         console.log("point1:" +point1+" point2:"+point2);
-    // //         svg.append('path')
-    // //         .attr('d', `M${triangleCenter.join(',')}L${point1.join(',')}L${point2.join(',')}Z`)
-    // //         .attr('fill', 'yellow')
-    // //         .attr('opacity', 1); 
-
-    // //         circle
-    // //         .attr('cx', point1[0])
-    // //         .attr('cy', point1[1])
-    // //         .attr('r', 10)
-    // //         .attr('fill', 'red'); 
-
-    // //          circle2
-    // //         .attr('cx', point2[0])
-    // //         .attr('cy', point2[1])
-    // //         .attr('r', 10)
-    // //         .attr('fill', 'blue'); 
-
-    // //         point1=point2;
-    // //         point2=minpoints[j];
-            
-    // //     }
-
-
-    //we need 3 ocatgons- 1 for first 8 hours, one for next 8 hours, one for last 8 hours
-//first, one loop does the full hours. we want it to iterate starting from the first point, and draw the number of triangles=the number of full hours. 
-//so obv hours%8;
-//add extra triangle for unfinished hour
